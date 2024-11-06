1c5dcc6d4b5c6af3b57a7722237d2382950d94e9
Md Tahmeed <117150654+mdtahmee@users.noreply.github.com>
2024-11-06 16:29:12 +0530

    Delete Untitled-1.java

--- Untitled-1.java
+++
@@ -1,7 +0,0 @@
-class String Buffer{
-    public static void main(String args[]){
-        String Buffer sb = new String Buffer("Hello");
-        sb.append("tahmeed");
-        System.out.println(sb);
-    }
-}
